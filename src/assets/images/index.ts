40cd2a8d36a0589ad5cde301c683cb72f7ea9349
Dapo Paul Ogunlana <dapoogunlana@gmail.com>
2022-07-03 20:07:26 +0100

    Image  import and placement updated

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,207 @@
 import Logo from './logo.png';
+import FooterLogo from './footer-logo.png';
+
 import BlueBrickBg from './blue-brick-bg.jpg';
+import YellowBrickBg from './yellow-brick-bg.jpg';
+import WhiteConcreteBg from './white-concrete-bg.jpg';
+import BigAngleBg from './big-angle-bg.jpg';
+import SmallAngleBg from './small-angle-bg.jpg';
+import FlipLineBg from './flip-line-bg.jpg';
+import YellowAngleBackdrop from './yellow-angle-backdrop.svg';
+
+import AboutImg from './about-img.png';
+import BannerImg1 from './banner-img-1.png';
+import BannerImg2 from './banner-img-2.jpg';
+import BannerImg3 from './banner-img-3.png';
+import DiscoverBtcImg from './discover-btc-img.jpg';
+import ManillaCard from './manilla-card.png';
+import ManillaCoin from './manilla-coin.png';
+import NewsImg from './news-img.jpg';
+import OurTech from './our-tech.jpg';
+import PolicyImg from './policy-img.png';
+import ReferralPhone from './referral-phone.png';
+import RegImg from './reg-image.png';
+import Testimonial1 from './testimonial-1.png';
+import Testimonial2 from './testimonial-2.png';
+import Testimonial3 from './testimonial-3.png';
+import UnlockingUtilitiesPhone from './unlocking-utilities-phone.png';
+import UnlockingUtilitiesPoints from './unlocking-utilities-points.png';
+import UnlockingUtilitiesStats from './unlocking-utilities-stats.png';
+import waysToTradeImg from './ways-to-trade-img.png';
+import HowToInnerRing from './how-to-inner-ring.svg';
+import HowToOuterRing from './how-to-outer-ring.svg';
+
+import AppleButton from './icons/apple-button.svg';
+import AppleIcon from './icons/apple-icon.svg';
+import PlaystoreButton from './icons/playstore-button.svg';
+import PlaystoreIcon from './icons/playstore-icon.svg';
+import BulletKiteIcon from './icons/bullet-kite-icon.svg';
+import ExchangeIconEtherium from './icons/exchange-icon-etherium.png';
+import HeroIcon1 from './icons/hero-icon-1.png';
+import HeroIcon2 from './icons/hero-icon-2.png';
+import HeroIcon3 from './icons/hero-icon-3.png';
+import HeroIcon4 from './icons/hero-icon-4.png';
+import HeroIcon5 from './icons/hero-icon-5.png';
+import HeroIcon6 from './icons/hero-icon-6.png';
+import HowToIconCreateAccount from './icons/how-to-icon-create-account.svg';
+import HowToIconEasyBuy from './icons/how-to-icon-easy-buy.svg';
+import HowToIconKYC from './icons/how-to-icon-kyc.svg';
+import ServiceIconAirtimeB from './icons/service-icon-airtime-b.svg';
+import ServiceIconBorrowB from './icons/service-icon-borrow-b.svg';
+import ServiceIconGiftCardB from './icons/service-icon-gift-card-b.svg';
+import ServiceIconHighApyB from './icons/service-icon-high-apy-b.svg';
+import ServiceIconP2PB from './icons/service-icon-p2p-b.svg';
+import ServiceIconUtilitiesB from './icons/service-icon-utilities-b.svg';
+import ServiceIconAirtimeW from './icons/service-icon-airtime-w.svg';
+import ServiceIconBorrowW from './icons/service-icon-borrow-w.svg';
+import ServiceIconGiftCardW from './icons/service-icon-gift-card-w.svg';
+import ServiceIconHighApyW from './icons/service-icon-high-apy-w.svg';
+import ServiceIconP2PW from './icons/service-icon-p2p-w.svg';
+import ServiceIconUtilitiesW from './icons/service-icon-utilities-w.svg';
+import TradesIconBankTransfers from './icons/trades-icon-bank-transfers.svg';
+import TradesIconCash from './icons/trades-icon-cash.svg';
+import TradesIconDebitCard from './icons/trades-icon-debit-card.svg';
+import TradesIconDigitalCurency from './icons/trades-icon-digital-curency.svg';
+import TradesIconGiftCard from './icons/trades-icon-gift-card.svg';
+import TradesIconOnlineWallet from './icons/trades-icon-online-wallet.svg';
+import WhyChooseIcon24Support from './icons/why-choose-icon-24-support.svg';
+import WhyChooseIconHighSpeed from './icons/why-choose-icon-high-speed.svg';
+import WhyChooseIconIntuitive from './icons/why-choose-icon-intuitive.svg';
+import WhyChooseIconSecurity from './icons/why-choose-icon-security.svg';
+
+import MediaCompanyBlockchainNews from './media-companies/media-company-blockchain-news.png';
+import MediaCompanyCoindesk from './media-companies/media-company-coindesk.png';
+import MediaCompanyCoingreek from './media-companies/media-company-coingreek.png';
+import MediaCompanyCoinmagaze from './media-companies/media-company-coinmagaze.png';
+import MediaCompanyDecrypt from './media-companies/media-company-decrypt.png';
+import MediaCompanyDigitalTrends from './media-companies/media-company-digital-trends.png';
+import MediaCompanyForbs from './media-companies/media-company-forbs.png';
+import MediaCompanyIrishTech from './media-companies/media-company-irish-tech.png';
+import MediaCompanyNasdaq from './media-companies/media-company-nasdaq.png';
+import MediaCompanyNewsBTC from './media-companies/media-company-news-btc.png';
+import MediaCompanyVoa from './media-companies/media-company-voa.png';
+import MediaCompanyYahoo from './media-companies/media-company-yahoo.png';
+
+import Operator9mobil from './operators/operator-9mobile.png';
+import OperatorGlo from './operators/operator-glo.png';
+import OperatorMtn from './operators/operator-mtn.png';
+import OperatorSurfline from './operators/operator-surfline.png';
+import OperatorVodafone from './operators/operator-vodafone.png';
+
+import AdviserCosti from './team/adviser-costi.jpg';
+import AdviserMar from './team/adviser-mar.jpg';
+import TeamAdeshina from './team/team-adeshina.jpg';
+import TeamBen from './team/team-ben.jpg';
+import TeamDapo from './team/team-dapo.jpg';
+import TeamEliko from './team/team-eliko.jpg';
+import TeamEtenikang from './team/team-etenikang.jpg';
+import TeamMohammad from './team/team-mohammad.jpg';
+import TeamObinna from './team/team-obinna.jpg';
+import TeamOyakhire from './team/team-oyakhire.jpg';
+import TeamRutherford from './team/team-rutherford.jpg';
+import TeamTunde from './team/team-tunde.jpg';
 
 export {
     Logo,
+    FooterLogo,
+
     BlueBrickBg,
+    YellowBrickBg,
+    WhiteConcreteBg,
+    BigAngleBg,
+    SmallAngleBg,
+    FlipLineBg,
+    YellowAngleBackdrop,
+
+    AboutImg,
+    BannerImg1,
+    BannerImg2,
+    BannerImg3,
+    DiscoverBtcImg,
+    ManillaCard,
+    ManillaCoin,
+    NewsImg,
+    OurTech,
+    PolicyImg,
+    ReferralPhone,
+    RegImg,
+    Testimonial1,
+    Testimonial2,
+    Testimonial3,
+    UnlockingUtilitiesPhone,
+    UnlockingUtilitiesPoints,
+    UnlockingUtilitiesStats,
+    waysToTradeImg,
+    HowToInnerRing,
+    HowToOuterRing,
+
+    AppleButton,
+    AppleIcon,
+    PlaystoreButton,
+    PlaystoreIcon,
+    BulletKiteIcon,
+    ExchangeIconEtherium,
+    HeroIcon1,
+    HeroIcon2,
+    HeroIcon3,
+    HeroIcon4,
+    HeroIcon5,
+    HeroIcon6,
+    HowToIconCreateAccount,
+    HowToIconEasyBuy,
+    HowToIconKYC,
+    ServiceIconAirtimeB,
+    ServiceIconBorrowB,
+    ServiceIconGiftCardB,
+    ServiceIconHighApyB,
+    ServiceIconP2PB,
+    ServiceIconUtilitiesB,
+    ServiceIconAirtimeW,
+    ServiceIconBorrowW,
+    ServiceIconGiftCardW,
+    ServiceIconHighApyW,
+    ServiceIconP2PW,
+    ServiceIconUtilitiesW,
+    TradesIconBankTransfers,
+    TradesIconCash,
+    TradesIconDebitCard,
+    TradesIconDigitalCurency,
+    TradesIconGiftCard,
+    TradesIconOnlineWallet,
+    WhyChooseIcon24Support,
+    WhyChooseIconHighSpeed,
+    WhyChooseIconIntuitive,
+    WhyChooseIconSecurity,
+
+    MediaCompanyBlockchainNews,
+    MediaCompanyCoindesk,
+    MediaCompanyCoingreek,
+    MediaCompanyCoinmagaze,
+    MediaCompanyDecrypt,
+    MediaCompanyDigitalTrends,
+    MediaCompanyForbs,
+    MediaCompanyIrishTech,
+    MediaCompanyNasdaq,
+    MediaCompanyNewsBTC,
+    MediaCompanyVoa,
+    MediaCompanyYahoo,
+
+    Operator9mobil,
+    OperatorGlo,
+    OperatorMtn,
+    OperatorSurfline,
+    OperatorVodafone,
+
+    AdviserCosti,
+    AdviserMar,
+    TeamAdeshina,
+    TeamBen,
+    TeamDapo,
+    TeamEliko,
+    TeamEtenikang,
+    TeamMohammad,
+    TeamObinna,
+    TeamOyakhire,
+    TeamRutherford,
+    TeamTunde,
 }
